8dee8b1e063c701177b1df9297a16e34cca22de1
nmaresco <nmarsco@bu.edu>
2016-04-09 11:02:58 -0400

    added GUI stubs

---
+++ Playlist/main.cpp
@@ -0,0 +1,15 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+
+    // set the intial conditions for the window!
+    w.setStyleSheet("background-color:rgb(204, 238, 255);");
+
+
+    w.show();
+    return a.exec();
+}
