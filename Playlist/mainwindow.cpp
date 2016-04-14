0d9131e2241a255b418b53b590b6a7495efa85ae
nmaresco <nmarsco@bu.edu>
2016-04-14 11:19:21 -0400

    added a list in the top 8 view

--- Playlist/mainwindow.cpp
+++ Playlist/mainwindow.cpp
@@ -1,14 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QListView>
+#include <QStringList>
+#include <QAbstractItemModel>
+#include <QStringListModel>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+QAbstractItemModel *buildModel(){
+    QStringList stringList;
+
+    for(int ii = 0; ii < 9; ii++){
+        QString temp = "Playlist " + QString::number(ii);
+        stringList << temp;
+    }
+
+    QStringListModel *model = new QStringListModel(stringList);
+    return model;
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::MainWindow(QWidget *parent) :QMainWindow(parent),ui(new Ui::MainWindow){
+    ui->setupUi(this);
+    QListView *listView = ui->mostPopularPlaylistListView;
+    QAbstractItemModel *model = buildModel();
+    listView->setModel(model);
+    listView->show();
+
+}
+
+MainWindow::~MainWindow(){
     delete ui;
 }
